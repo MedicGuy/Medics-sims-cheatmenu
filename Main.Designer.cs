847b573ae51351ebbd2dd7c099bb0b8d3471e7dc
MedicGuy <54997917+MedicGuy@users.noreply.github.com>
2020-08-21 15:55:08 +0200

    V1

---
+++ Main.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace MedicSimsCheatmenu
+{
+    partial class MedicsCheatmenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_moveobjects = new System.Windows.Forms.Button();
+            this.bt_showhiddenobjects = new System.Windows.Forms.Button();
+            this.bt_showliveeditobjects = new System.Windows.Forms.Button();
+            this.bt_ignoregameplayunlocksentitlement = new System.Windows.Forms.Button();
+            this.cas_fulleditmode = new System.Windows.Forms.Button();
+            this.bt_testingcheats = new System.Windows.Forms.Button();
+            this.bt_money = new System.Windows.Forms.Button();
+            this.bt_cas_unlockbytag = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt_moveobjects
+            // 
+            this.bt_moveobjects.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.bt_moveobjects.Location = new System.Drawing.Point(12, 56);
+            this.bt_moveobjects.Name = "bt_moveobjects";
+            this.bt_moveobjects.Size = new System.Drawing.Size(123, 26);
+            this.bt_moveobjects.TabIndex = 0;
+            this.bt_moveobjects.Text = "bb.moveobjects";
+            this.bt_moveobjects.UseVisualStyleBackColor = false;
+            this.bt_moveobjects.Click += new System.EventHandler(this.bt_moveobjects_Click);
+            // 
+            // bt_showhiddenobjects
+            // 
+            this.bt_showhiddenobjects.Location = new System.Drawing.Point(12, 88);
+            this.bt_showhiddenobjects.Name = "bt_showhiddenobjects";
+            this.bt_showhiddenobjects.Size = new System.Drawing.Size(123, 26);
+            this.bt_showhiddenobjects.TabIndex = 1;
+            this.bt_showhiddenobjects.Text = "bb.showhiddenobjects";
+            this.bt_showhiddenobjects.UseVisualStyleBackColor = true;
+            this.bt_showhiddenobjects.Click += new System.EventHandler(this.bt_showhiddenobjects_Click);
+            // 
+            // bt_showliveeditobjects
+            // 
+            this.bt_showliveeditobjects.Location = new System.Drawing.Point(141, 56);
+            this.bt_showliveeditobjects.Name = "bt_showliveeditobjects";
+            this.bt_showliveeditobjects.Size = new System.Drawing.Size(193, 26);
+            this.bt_showliveeditobjects.TabIndex = 2;
+            this.bt_showliveeditobjects.Text = "bb.showliveeditobjects";
+            this.bt_showliveeditobjects.UseVisualStyleBackColor = true;
+            this.bt_showliveeditobjects.Click += new System.EventHandler(this.bt_showliveeditobjects_Click);
+            // 
+            // bt_ignoregameplayunlocksentitlement
+            // 
+            this.bt_ignoregameplayunlocksentitlement.Location = new System.Drawing.Point(141, 88);
+            this.bt_ignoregameplayunlocksentitlement.Name = "bt_ignoregameplayunlocksentitlement";
+            this.bt_ignoregameplayunlocksentitlement.Size = new System.Drawing.Size(193, 26);
+            this.bt_ignoregameplayunlocksentitlement.TabIndex = 3;
+            this.bt_ignoregameplayunlocksentitlement.Text = "bb.ignoregameplayunlocksentitlement";
+            this.bt_ignoregameplayunlocksentitlement.UseVisualStyleBackColor = true;
+            this.bt_ignoregameplayunlocksentitlement.Click += new System.EventHandler(this.bt_ignoregameplayunlocksentitlement_Click);
+            // 
+            // cas_fulleditmode
+            // 
+            this.cas_fulleditmode.Location = new System.Drawing.Point(141, 136);
+            this.cas_fulleditmode.Name = "cas_fulleditmode";
+            this.cas_fulleditmode.Size = new System.Drawing.Size(193, 25);
+            this.cas_fulleditmode.TabIndex = 4;
+            this.cas_fulleditmode.Text = "cas.fulleditmode";
+            this.cas_fulleditmode.UseVisualStyleBackColor = true;
+            this.cas_fulleditmode.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // bt_testingcheats
+            // 
+            this.bt_testingcheats.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.bt_testingcheats.Location = new System.Drawing.Point(13, 12);
+            this.bt_testingcheats.Name = "bt_testingcheats";
+            this.bt_testingcheats.Size = new System.Drawing.Size(321, 38);
+            this.bt_testingcheats.TabIndex = 5;
+            this.bt_testingcheats.Text = "testingcheats";
+            this.bt_testingcheats.UseVisualStyleBackColor = false;
+            this.bt_testingcheats.Click += new System.EventHandler(this.bt_testingcheats_Click);
+            // 
+            // bt_money
+            // 
+            this.bt_money.Location = new System.Drawing.Point(12, 167);
+            this.bt_money.Name = "bt_money";
+            this.bt_money.Size = new System.Drawing.Size(322, 25);
+            this.bt_money.TabIndex = 6;
+            this.bt_money.Text = "money 500,000/5000";
+            this.bt_money.UseVisualStyleBackColor = true;
+            this.bt_money.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bt_cas_unlockbytag
+            // 
+            this.bt_cas_unlockbytag.Location = new System.Drawing.Point(12, 136);
+            this.bt_cas_unlockbytag.Name = "bt_cas_unlockbytag";
+            this.bt_cas_unlockbytag.Size = new System.Drawing.Size(123, 25);
+            this.bt_cas_unlockbytag.TabIndex = 8;
+            this.bt_cas_unlockbytag.Text = "cas.unlockbytag";
+            this.bt_cas_unlockbytag.UseVisualStyleBackColor = true;
+            this.bt_cas_unlockbytag.Click += new System.EventHandler(this.bt_cas_unlockbytag_Click);
+            // 
+            // MedicsCheatmenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 201);
+            this.Controls.Add(this.bt_cas_unlockbytag);
+            this.Controls.Add(this.bt_money);
+            this.Controls.Add(this.bt_testingcheats);
+            this.Controls.Add(this.bt_ignoregameplayunlocksentitlement);
+            this.Controls.Add(this.cas_fulleditmode);
+            this.Controls.Add(this.bt_showliveeditobjects);
+            this.Controls.Add(this.bt_showhiddenobjects);
+            this.Controls.Add(this.bt_moveobjects);
+            this.Name = "MedicsCheatmenu";
+            this.Text = "Medic\'s Cheatmenu V1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_moveobjects;
+        private System.Windows.Forms.Button bt_showhiddenobjects;
+        private System.Windows.Forms.Button bt_showliveeditobjects;
+        private System.Windows.Forms.Button bt_ignoregameplayunlocksentitlement;
+        private System.Windows.Forms.Button cas_fulleditmode;
+        private System.Windows.Forms.Button bt_testingcheats;
+        private System.Windows.Forms.Button bt_money;
+        private System.Windows.Forms.Button bt_cas_unlockbytag;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MedicSimsCheatmenu
+{
+    public partial class MedicsCheatmenu : Form
+    {
+        [DllImport("user32")]
+        private static extern bool SetForegroundWindow(IntPtr hwnd);
+        public bool bool_button1, bool_button2, bool_button3, bool_button4, bool_button5, bool_button6, bool_button7, bool_button8;
+        public MedicsCheatmenu()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_cas_unlockbytag_Click(object sender, EventArgs e)
+        {
+            var proc = Process.GetProcessesByName("TS4_x64").FirstOrDefault();
+            SetForegroundWindow(proc.MainWindowHandle);
+            if (proc != null && proc.MainWindowHandle != IntPtr.Zero)
+            {
+                Thread.Sleep(500);
+                Console.Beep(500, 250);
+                SendKeys.SendWait("^(+c)");
+                Thread.Sleep(50);
+                SendKeys.SendWait("cas.unlockbytag{enter}");
+                Thread.Sleep(200);
+                SendKeys.SendWait("^(+c)");
+                if (bool_button8 == false)
+                {
+                    bt_cas_unlockbytag.BackColor = SystemColors.GradientActiveCaption;
+                    bool_button8 = true;
+                }
+                else if (bool_button8 == true)
+                {
+                    bt_cas_unlockbytag.BackColor = SystemColors.ControlLight;
+                    bool_button8 = false;
+                }
+            }
+        }
+
+        private void bt_moveobjects_Click(object sender, EventArgs e)
+        {
+            var proc = Process.GetProcessesByName("TS4_x64").FirstOrDefault();
+            SetForegroundWindow(proc.MainWindowHandle);
+            if (proc != null && proc.MainWindowHandle != IntPtr.Zero)
+            {
+                Thread.Sleep(500);
+                Console.Beep(500, 250);
+                SendKeys.SendWait("^(+c)");
+                Thread.Sleep(50);
+                SendKeys.SendWait("bb.moveobjects {enter}");
+                Thread.Sleep(200);
+                SendKeys.SendWait("^(+c)");
+                if (bool_button1 == false)
+                {
+                    bt_moveobjects.BackColor = SystemColors.GradientActiveCaption;
+                    bool_button1 = true;
+                }
+                else if (bool_button1 == true)
+                {
+                    bt_moveobjects.BackColor = SystemColors.ControlLight;
+                    bool_button1 = false;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var proc = Process.GetProcessesByName("TS4_x64").FirstOrDefault();
+            SetForegroundWindow(proc.MainWindowHandle);
+            if (proc != null && proc.MainWindowHandle != IntPtr.Zero)
+            {
+                Thread.Sleep(500);
+                Console.Beep(500, 250);
+                SendKeys.SendWait("^(+c)");
+                Thread.Sleep(50);
+                if (bool_button7 == false)
+                {
+                    bt_money.BackColor = SystemColors.GradientActiveCaption;
+                    SendKeys.SendWait("money 500000{enter}");
+                    bool_button7 = true;
+                }
+                else if (bool_button7 == true)
+                {
+                    bt_money.BackColor = SystemColors.ControlLight;
+                    SendKeys.SendWait("money 5000{enter}");
+                    bool_button7 = false;
+                }
+                Thread.Sleep(200);
+                SendKeys.SendWait("^(+c)");
+            }
+        }
+
+        private void bt_testingcheats_Click(object sender, EventArgs e)
+        {
+            var proc = Process.GetProcessesByName("TS4_x64").FirstOrDefault();
+            SetForegroundWindow(proc.MainWindowHandle);
+            if (proc != null && proc.MainWindowHandle != IntPtr.Zero)
+            {
+                Thread.Sleep(500);
+                Console.Beep(500, 250);
+                SendKeys.SendWait("^(+c)");
+                Thread.Sleep(50);
+                if (bool_button6 == false)
+                {
+                    SendKeys.SendWait("testingcheats true{enter}");
+                    bt_testingcheats.BackColor = SystemColors.GradientActiveCaption;
+                    bool_button6 = true;
+                }
+                else if (bool_button6 == true)
+                {
+                    SendKeys.SendWait("testingcheats false{enter}");
+                    bt_testingcheats.BackColor = SystemColors.ControlLight;
+                    bool_button6 = false;
+                }
+                Thread.Sleep(200);
+                SendKeys.SendWait("^(+c)");
+            }
+        }
+
+        private void bt_showhiddenobjects_Click(object sender, EventArgs e)
+        {
+            var proc = Process.GetProcessesByName("TS4_x64").FirstOrDefault();
+            SetForegroundWindow(proc.MainWindowHandle);
+            if (proc != null && proc.MainWindowHandle != IntPtr.Zero)
+            {
+                Thread.Sleep(500);
+                Console.Beep(500, 250);
+                SendKeys.SendWait("^(+c)");
+                Thread.Sleep(50);
+                SendKeys.SendWait("bb.showhiddenobjects{enter}");
+                Thread.Sleep(200);
+                SendKeys.SendWait("^(+c)");
+                if (bool_button2 == false)
+                {
+                    bt_showhiddenobjects.BackColor = SystemColors.GradientActiveCaption;
+                    bool_button2 = true;
+                }
+                else if (bool_button2 == true)
+                {
+                    bt_showhiddenobjects.BackColor = SystemColors.ControlLight;
+                    bool_button2 = false;
+                }
+            }
+        }
+
+        private void bt_showliveeditobjects_Click(object sender, EventArgs e)
+        {
+            var proc = Process.GetProcessesByName("TS4_x64").FirstOrDefault();
+            SetForegroundWindow(proc.MainWindowHandle);
+            if (proc != null && proc.MainWindowHandle != IntPtr.Zero)
+            {
+                Thread.Sleep(500);
+                Console.Beep(500, 250);
+                SendKeys.SendWait("^(+c)");
+                Thread.Sleep(50);
+                SendKeys.SendWait("bb.showliveeditobjects{enter}");
+                Thread.Sleep(200);
+                SendKeys.SendWait("^(+c)");
+                if (bool_button3 == false)
+                {
+                    bt_showliveeditobjects.BackColor = SystemColors.GradientActiveCaption;
+                    bool_button3 = true;
+                }
+                else if (bool_button3 == true)
+                {
+                    bt_showliveeditobjects.BackColor = SystemColors.ControlLight;
+                    bool_button3 = false;
+                }
+            }
+        }
+
+        private void bt_ignoregameplayunlocksentitlement_Click(object sender, EventArgs e)
+        {
+            var proc = Process.GetProcessesByName("TS4_x64").FirstOrDefault();
+            SetForegroundWindow(proc.MainWindowHandle);
+            if (proc != null && proc.MainWindowHandle != IntPtr.Zero)
+            {
+                Thread.Sleep(500);
+                Console.Beep(500, 250);
+                SendKeys.SendWait("^(+c)");
+                Thread.Sleep(50);
+                SendKeys.SendWait("bb.ignoregameplayunlocksentitlement{enter}");
+                Thread.Sleep(200);
+                SendKeys.SendWait("^(+c)");
+                if (bool_button4 == false)
+                {
+                    bt_ignoregameplayunlocksentitlement.BackColor = SystemColors.GradientActiveCaption;
+                    bool_button4 = true;
+                }
+                else if (bool_button4 == true)
+                {
+                    bt_ignoregameplayunlocksentitlement.BackColor = SystemColors.ControlLight;
+                    bool_button4 = false;
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var proc = Process.GetProcessesByName("TS4_x64").FirstOrDefault();
+            SetForegroundWindow(proc.MainWindowHandle);
+            if (proc != null && proc.MainWindowHandle != IntPtr.Zero)
+            {
+                Thread.Sleep(500);
+                Console.Beep(500, 250);
+                SendKeys.SendWait("^(+c)");
+                Thread.Sleep(50);
+                SendKeys.SendWait("cas.fulleditmode{enter}");
+                Thread.Sleep(200);
+                SendKeys.SendWait("^(+c)");
+                if (bool_button5 == false)
+                {
+                    cas_fulleditmode.BackColor = SystemColors.GradientActiveCaption;
+                    bool_button5 = true;
+                }
+                else if (bool_button5 == true)
+                {
+                    cas_fulleditmode.BackColor = SystemColors.ControlLight;
+                    bool_button5 = false;
+                }
+            }
+        }
+    }
+}
